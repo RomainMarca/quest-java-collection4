e4e3600a8abc66537fb33300c794f9a272da7e59
Romain MARCA <romain.marca@sii.fr>
2019-06-18 15:17:16 +0200

    first and last commit

--- Thanos.java
+++ Thanos.java
@@ -1,15 +1,19 @@
+import java.util.Comparator;
+import java.util.TreeMap;
+import java.util.Map;
+
 public class Thanos {
-
     public static void main(String[] args) {
+  
 
-        Hero hulk = new Hero("Hulk", 49);
+     Hero hulk = new Hero("Hulk", 49);
         Hero blackWidow = new Hero("Black Widow", 34);
         Hero captainAmerica = new Hero("Captain America", 100);
         Hero thor = new Hero("Thor", 1501);
         Hero vision = new Hero("Vision", 3);
         Hero scarletWitch = new Hero("Scarlet Witch", 29);
         Hero doctorStrange = new Hero("Doctor Strange", 42);
-;
+        ;
         Flower lily = new Flower("Lily");
         Flower tulip = new Flower("Tulip");
         Flower begonia = new Flower("Begonia");
@@ -22,9 +26,30 @@ public class Thanos {
         // Captain America has a Lily
         // Doctor Strange and Black Widow have a Violet
 
-        // TODO 2 : Print if `begonia` is contained in the TreeMap
+        Comparator<Hero> comparator = new Comparator<Hero>() {
+            @Override
+            public int compare(Hero orig, Hero compare) {
+                return orig.getName().compareTo(compare.getName());
+            }
+        };
 
+        Map<Hero, Flower> party = new TreeMap<>(comparator);
+
+        party.put(hulk, rose);
+        party.put(thor, rose);
+        party.put(scarletWitch, rose);
+        party.put(vision, tulip);
+        party.put(captainAmerica, lily);
+        party.put(doctorStrange, violet);
+        party.put(blackWidow, violet);
+
+        // TODO 2 : Print if `begonia` is contained in the TreeMap
+        System.out.println(party.containsValue(begonia));
         // TODO 3 : For each hero, alphabetically, print the corresponding flower
+        for (Hero heros : party.keySet()) {
+            Flower flower = party.get(heros);
+            System.out.println(flower.getName());
+        }
 
     }
 }
